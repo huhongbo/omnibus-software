cba1d4dee12c4ce3fa1d686a79b5f6e8fd6234ac
huhongbo <huhongbo@gmail.com>
2013-04-16 11:08:37 +0800

    add libpcap

---
+++ config/software/libpcap.rb
@@ -0,0 +1,39 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "libpcap"
+version "0.9.8"
+
+source :url => 'http://www.tcpdump.org/release/libpcap-0.9.8.tar.gz',
+       :md5 => '5208f24d0328ee7c20b52c43eaa9aa0e'
+
+relative_path "libpcap-0.9.8"
+
+env = {
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+if platform == "solaris2"
+  env.merge!({"LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc", "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"})
+end
+
+build do
+  command "./configure --prefix=#{install_dir}/embedded", :env => env
+  command "make -j #{max_build_jobs}", :env => env
+  command "make install", :env => env
+end
--- config/software/mongodb.rb
+++ config/software/mongodb.rb
@@ -18,7 +18,7 @@
 name "mongodb"
 version "2.4.1"
 
-dependencies ["rsync"]
+dependencies ["rsync", "libpcap"]
 
 source :url => "http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.4.1.tgz",
        :md5 => "bda7d448143cd5227c640b972f8873de"
