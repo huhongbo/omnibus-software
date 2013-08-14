3ff169c662ceffda4357ab304e862dcf88e95e28
huhongbo <huhongbo@gmail.com>
2013-08-14 12:53:35 +0800

    libpng bug fix

--- config/software/libpng.rb
+++ config/software/libpng.rb
@@ -29,8 +29,8 @@ configure_env = {
   "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
   "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
   "CPPFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
   "PKG_CONFIG_PATH" => "#{install_dir}/embedded/lib/pkgconfig"
 }
 
